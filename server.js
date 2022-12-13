5db418522e0c0459b3937e77ece211717cc13a70
desta2000 desta2000 <shlomodesta1@gmail.com>
2022-12-13 20:18:15 +0200

    Evn Addition a few workaround

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+
+require('dotenv').config();
+function SendMail(u,p)
+{
+    console.log("sent ")
+}
+SendMail(process.env.USER_EMAIL,process.env.PASSWORD)
 const http = require('http');
 const app = require('./app');
 const port = 2200;
